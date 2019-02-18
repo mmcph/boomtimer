f900cfa7eba3b03824212da8d9aa26d237853197
Marlon McPherson <mmcpherson5@cnm.edu>
2019-02-17 23:06:46 -0700

    Wrote colon display rules. Timer now starts at 0.

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
-
+const colonHours = document.getElementById('colon-1');
+const colonMinutes = document.getElementById('colon-2');
 
 setInterval(() => {
   let seconds = document.getElementById('seconds-1');
@@ -7,14 +8,18 @@ setInterval(() => {
   let secondsVal = seconds.innerHTML;
   let minutesVal = minutes.innerHTML;
   let hoursVal   = hours.innerHTML;
-  if (secondsVal == 59) {
+  if (!secondsVal) {
+    seconds.innerHTML = '01';
+  } else if (secondsVal == 59) {
     seconds.innerHTML = '00';
     if (!minutesVal) {
       minutes.innerHTML = '01';
+      colonMinutes.innerHTML = ':'
     } else if (minutesVal == 59) {
       minutes.innerHTML = '00';
       if (!hoursVal) {
         hours.innerHTML = '01';
+        colonHours.innerHTML = ':'
       } else if (-1 < hoursVal && hoursVal < 9) {
         hoursVal++;
         hours.innerHTML = `0${hoursVal}`;
